0fb6d7c620b00310b86c4d1743b2158fa784f1fc
Erik Villavera <villaveraerik@gmail.com>
2020-09-11 15:35:50 -0400

    Chapter 18 done

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        startNewRound()
+        startNewGame()
     }
     
     func startNewRound(){
@@ -34,6 +34,12 @@ class ViewController: UIViewController {
         updateLabels()
     }
     
+    @IBAction func startNewGame(){
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
     func updateLabels(){
         targetLabel.text = String(targetValue)
         scoreLabel.text = String(score)
@@ -42,22 +48,37 @@ class ViewController: UIViewController {
 
     @IBAction func showAlert() {
         let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
         
-        let points = 100 - difference
+        
+        let title: String
+        if difference == 0{
+            title = "Perfect!"
+            points += 100
+        }else if difference < 5 {
+            title = "You almost had it!"
+            if(difference == 1){
+                points += 50
+            }
+        }else if difference < 10 {
+            title = "Pretty good!"
+        }else{
+            title = "Not even close..."
+        }
         
         score += points
         
         let message = "You scored \(points) points"
         
-        let alert = UIAlertController(title:"Hello, World", message: message,preferredStyle: .alert)
+        let alert = UIAlertController(title: title, message: message,preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "OK", style:.default, handler: nil)
+        let action = UIAlertAction(title: "OK", style:.default, handler: {_ in
+            self.startNewRound()
+        })
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-        
-        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider){
