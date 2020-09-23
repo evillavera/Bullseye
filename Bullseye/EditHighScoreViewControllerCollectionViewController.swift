8d041a0ba4aafd5e9c4a68fa62c069d0f5929540
Erik Villavera <villaveraerik@gmail.com>
2020-09-23 09:45:10 -0400

    Chapter 23 done

---
+++ Bullseye/EditHighScoreViewControllerCollectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EditHighScoreViewControllerCollectionViewController.swift
+//  Bullseye
+//
+//  Created by Erik Villavera on 9/22/20.
+//  Copyright © 2020 Erik Villavera. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class EditHighScoreViewControllerCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
